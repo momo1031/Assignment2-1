2f9fe053ca072b09ef78e7701a704d1658a8a3d2
jipgazua <wjdehreh@naver.com>
2019-04-02 16:35:02 +0900

    my assign

--- array_test.sh
+++ array_test.sh
@@ -1,1 +1,18 @@
-abc
+#!/bin/bash
+
+i=0
+declare -a alist
+while [ 1 ]
+do
+read -p "Enter a new word :" word
+
+if [ "$word" = "quit" ]; then
+exit 1
+elif [ "$word" = "list" ]; then
+echo ${alist[*]}
+else
+alist+=($word)
+let i=$i+1
+echo "Length: $i"
+fi
+done
